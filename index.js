aa12a98aecd72638dfa1777f0160fd318aa3cb2f
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-06-03 13:52:53 -0700

    uh duh

--- index.js
+++ index.js
@@ -5,25 +5,25 @@
 
 var Rainbow = require('./js/rainbow')
   , languages = [
-      require('.js/language/c.js'),
-      require('.js/language/coffeescript.js'),
-      require('.js/language/csharp.js'),
-      require('.js/language/css.js'),
-      require('.js/language/d.js'),
-      require('.js/language/generic.js'),
-      require('.js/language/go.js'),
-      require('.js/language/haskell.js'),
-      require('.js/language/html.js'),
-      require('.js/language/java.js'),
-      require('.js/language/javascript.js'),
-      require('.js/language/lua.js'),
-      require('.js/language/php.js'),
-      require('.js/language/python.js'),
-      require('.js/language/r.js'),
-      require('.js/language/ruby.js'),
-      require('.js/language/scheme.js'),
-      require('.js/language/shell.js'),
-      require('.js/language/smalltalk.js')
+      require('./js/language/c.js'),
+      require('./js/language/coffeescript.js'),
+      require('./js/language/csharp.js'),
+      require('./js/language/css.js'),
+      require('./js/language/d.js'),
+      require('./js/language/generic.js'),
+      require('./js/language/go.js'),
+      require('./js/language/haskell.js'),
+      require('./js/language/html.js'),
+      require('./js/language/java.js'),
+      require('./js/language/javascript.js'),
+      require('./js/language/lua.js'),
+      require('./js/language/php.js'),
+      require('./js/language/python.js'),
+      require('./js/language/r.js'),
+      require('./js/language/ruby.js'),
+      require('./js/language/scheme.js'),
+      require('./js/language/shell.js'),
+      require('./js/language/smalltalk.js')
     ];
 
 
